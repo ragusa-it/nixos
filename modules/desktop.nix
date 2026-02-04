25a0a24ca7a5cbe21a8741372ad2e6fbcac042ce
Melvin Ragusa <kontakt@ragusa-it.dev>
2026-02-04 22:14:47 +0100

    Update desktop.nix

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -16,12 +16,14 @@
   xdg.portal = {
     enable = true;
     extraPortals = [
-      pkgs.xdg-desktop-portal-gnome
       pkgs.xdg-desktop-portal-gtk
     ];
     config.common = {
       default = [ "gtk" ];
       "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+    };
+    config.niri = {
+      default = [ "gtk" ];
     };
   };
 
