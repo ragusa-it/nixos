9dbc9679f99bf023567dd24657a17ce064f7696e
Melvin Ragusa <kontakt@ragusa-it.dev>
2026-02-04 13:58:15 +0100

    Update apps.nix

--- modules/apps.nix
+++ modules/apps.nix
@@ -51,6 +51,8 @@
     # ═══════════════════════════════════════════════════════════════
     file-roller # Archive manager (GUI)
     gnome-disk-utility # Disk management
+    gnome-online-accounts
+    gnome-gnome-online-accounts-gtk
 
     # Archive tools (for file-roller and CLI)
     unzip
